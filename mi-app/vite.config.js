973f85b5c6d1b940dc8deefefd6d9d537e3be3e1
diegolezcanodev <diegolezcano209@gmail.com>
2025-08-12 00:14:54 -0300

    build

--- mi-app/vite.config.js
+++ mi-app/vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  base: '/'
 })
